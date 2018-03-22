96966db5d386ad25757ab8dabf8c68ea2d5b950f
tarak7929 <tarak7929@gmail.com>
2018-03-22 17:08:33 -0400

    First Commit

---
+++ src/DataFile.java
@@ -0,0 +1,12 @@
+
+public class DataFile {
+	
+	Xls_Reader d = new Xls_Reader("C:\\QA Training\\Testing\\NikulTest.xlsx");
+	
+	public String validEmail = d.getCellData("Data1", "UserName", 2);
+	public String invalidEmail = d.getCellData("Data1", "UserName", 3);
+	public String invalidPassword = d.getCellData("Data1", "Password", 2);
+	public String emailError = d.getCellData("Data1", "Email Error", 2);
+	public String passwordError = d.getCellData("Data1", "Password Error", 2);
+
+}
